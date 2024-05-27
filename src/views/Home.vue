205d530d848a09af011ff9eac8e5aeba6edacce9
Claire Li <s3310507057@gmail.com>
2024-05-27 14:46:19 +0200

    feat: add destination pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>Home</div>
+  <div class="home">
+    <h1>All Destination</h1>
+    <div class="destinations">
+      <router-link
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="destination.slug"
+      >
+        <h2>{{ destination.name }}</h2>
+        <img :src="`/images/${destination.image}`" :alt="destination.name" />
+      </router-link>
+    </div>
+  </div>
 </template>
+
+<script>
+import sourceData from '@/data.json';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      destinations: sourceData.destinations,
+    };
+  },
+};
+</script>
