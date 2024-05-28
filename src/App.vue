3fde648dc281d291c88800989d748afc3da23a0e
Claire Li <s3310507057@gmail.com>
2024-05-28 13:25:55 +0200

    feat: add apge transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <TheNavigation />
   <div class="container">
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -14,3 +18,15 @@ export default {
   },
 };
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
